de09396911d8639b4f474934ac77ccc7a7c7d8be
Josh Kremer <joshbkremer@gmail.com>
2016-09-18 17:14:22 -0400

    Add in video changing

--- app/index.js
+++ app/index.js
@@ -1,17 +1,32 @@
+var http = require('http');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser')
 
 var app = express();
-var collectionDriver;
+var currentVideo = 'https://www.youtube.com/embed/8s3UogfAGg0';
 
-
-
+// parse application/json
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/', function(res) {
+  console.log("sup");
 });
 
-app.listen(process.env.PORT ||3000, function() {
+app.get('/currentVideo', function(req, res) {
+  res.send(200, {youtubeSrc: currentVideo});
+});
+
+app.post('/postVideo', function(req, res) {
+  currentVideo = req.body.newVideo;
+
+  res.send(200, {youtubeSrc: currentVideo});
+});
+
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Example app listening on port 3000!');
 });
