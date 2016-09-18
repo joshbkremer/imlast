f54916493e86be2d5acd5930c7b448e8c2a82aba
Josh Kremer <joshbkremer@gmail.com>
2016-09-18 01:09:05 -0400

    Add inital mongodb functionality

--- app/index.js
+++ app/index.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+var CollectionDriver = require('./collectionDriver').CollectionDriver;
+
 var app = express();
+var collectionDriver;
+
+//MongoClient.connect(process.env.MONGOHQ_URL, function(err, db){
+//  if (err) {
+//      console.error("Error! Exiting... Must start MongoDB first");
+//      process.exit(1);
+//  }
+//
+//  collectionDriver = new CollectionDriver(db); //F
+//});
+
+var mongoClient = new MongoClient(new Server('localHost', 27017));
+mongoClient.open(function(err, mongoClient) { //C
+  if (!mongoClient) {
+      console.error("Error! Exiting... Must start MongoDB first");
+      process.exit(1); //D
+  }
+  var db = mongoClient.db("imLastDB");  //E
+  collectionDriver = new CollectionDriver(db); //F
+});
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
