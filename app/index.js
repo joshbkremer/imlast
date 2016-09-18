c771712f7d0af68799eb2facc62ee6e024354381
Josh Kremer <joshbkremer@gmail.com>
2016-09-18 17:44:54 -0400

    Add in price changing

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser')
 
 var app = express();
 var currentVideo = 'https://www.youtube.com/embed/8s3UogfAGg0';
+var currentPrice = '2.00';
 
 // parse application/json
 // parse application/x-www-form-urlencoded
@@ -18,13 +19,16 @@ app.get('/', function(res) {
 });
 
 app.get('/currentVideo', function(req, res) {
-  res.send(200, {youtubeSrc: currentVideo});
+  res.send(200, {youtubeSrc: currentVideo, price: currentPrice});
 });
 
 app.post('/postVideo', function(req, res) {
   currentVideo = req.body.newVideo;
 
-  res.send(200, {youtubeSrc: currentVideo});
+  var priceDouble = parseFloat(currentPrice) + 1.00;
+  currentPrice = priceDouble.toString();
+
+  res.send(200, {youtubeSrc: currentVideo, price: currentPrice});
 });
 
 http.createServer(app).listen(app.get('port'), function() {
